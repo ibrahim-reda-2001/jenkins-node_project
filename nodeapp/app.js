01d204dbb0d580fc5122ea41b9a0ba9a10b1db2f
ibrahim <ibrahim@ibra.localdomain>
2024-12-14 17:43:41 +0200

    edit for me

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello Abdo ')
+    res.send('Hello NTI R9 ')
   })
   
   app.listen(port, () => {
